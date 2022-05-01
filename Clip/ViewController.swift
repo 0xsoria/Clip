8dac50b0d6a7d9457a1f82321cb6eb8329dbf306
Gabriel Sória <00.roomers-puzzled@icloud.com>
2022-05-01 19:52:42 +0100

    Initial Commit

---
+++ Clip/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Clip
+//
+//  Created by Gabriel Soria Souza on 01/05/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
